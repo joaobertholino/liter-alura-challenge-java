ae9300ffc22b62ac1f79788d717000d4839ce620
João Bertholino <joaobertholino964@gmail.com>
2024-11-04 20:40:33 -0300

    feat: spring initial project

---
+++ src/main/java/dev/joaobertholino/literalurachallengejava/LiteraluraChallengeJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.joaobertholino.literalurachallengejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraChallengeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraChallengeJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joaobertholino/literalurachallengejava/LiteraluraChallengeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaobertholino.literalurachallengejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraChallengeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
